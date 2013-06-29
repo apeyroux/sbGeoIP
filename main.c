ea02e137428730dfd03190cfeb9e37e8cae48b8f
Jean-Alexandre Peyroux <m@j4.pe>
2013-06-29 23:23:53 +0200

    first commit !

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <GeoIP.h>
+#include <GeoIPCity.h>
+
+int main(int argc, char *argv[])
+{
+	GeoIP * gi;
+	GeoIPRecord * gr;
+	gi = GeoIP_open("/usr/share/GeoIP/GeoLiteCity.dat", GEOIP_STANDARD);
+	gr = GeoIP_record_by_addr(gi, argv[1]);
+	printf("country: %s\n",GeoIP_country_name_by_addr(gi, argv[1]));
+	if(gr!=NULL)
+		printf("city: %s\n", gr->city);
+	return 0;
+}
